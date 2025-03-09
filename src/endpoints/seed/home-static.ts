9073a2ee984c92d218189444c1661d43516d8c93
Papol14 <johnpaulescalona2@gmail.com>
2025-03-09 15:28:24 +0800

    Some Changes

--- src/endpoints/seed/home-static.ts
+++ src/endpoints/seed/home-static.ts
@@ -61,7 +61,7 @@ export const homeStatic: RequiredDataFromCollectionSlug<'pages'> = {
                 format: 0,
                 mode: 'normal',
                 style: '',
-                text: ' to find your products that yyou like.',
+                text: ' to find your products that you like.',
                 version: 1,
               },
             ],
