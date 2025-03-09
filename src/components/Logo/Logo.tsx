d9dc675ad0e26d0862d6fa5b65107b8999832003
Papol14 <johnpaulescalona2@gmail.com>
2025-03-09 14:22:51 +0800

    Some changes

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -17,13 +17,13 @@ export const Logo = (props: Props) => {
     /* eslint-disable @next/next/no-img-element */
     <img
       alt="Payload Logo"
-      width={193}
-      height={34}
+      width={600} // Increased from 500 to 600
+      height={41} // Increased from 34 to 41
       loading={loading}
       fetchPriority={priority}
       decoding="async"
-      className={clsx('max-w-[9.375rem] w-full h-[34px]', className)}
-      src="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-logo-light.svg"
+      className={clsx('max-w-[20.375rem] w-full h-[30px]', className)}
+      src="icon.svg"
     />
   )
 }
